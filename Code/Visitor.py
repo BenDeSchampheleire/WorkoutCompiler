af4192d30244110e3bd548a3592e51277eab17fd
BenDeSchampheleire <ben.de.schampheleire@telenet.be>
2022-04-08 15:06:09 +0200

    Pretty Printer finished

--- Code/Visitor.py
+++ Code/Visitor.py
@@ -6,21 +6,25 @@ class Visitor:
         return program
 
     def visitProgram(self, program):
+        print("Visiting Program")
         program.name.accept(self)
         for workout in program.workouts:
             workout.accept(self)
         return program
 
     def visitWorkout(self, workout):
+        print("Visiting Workout")
         workout.name.accept(self)
         for exercise in workout.exercises:
             exercise.accept(self)
         return workout
 
     def visitExercise(self, exercise):
+        print("Visiting Exercise")
         exercise.name.accept(self)
         return exercise
 
     @staticmethod
     def visitName(name):
+        print("Visiting Name")
         return name
